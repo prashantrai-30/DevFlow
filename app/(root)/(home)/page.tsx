fa70cc63630c80ef836401e046d9bb01dd9dbb1a
prashantrai-30 <prashantkumar2488@gmail.com>
2025-06-08 09:36:54 +0530

    add assets and layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,13 @@
+
+import {SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <UserButton/>
+      <SignUpButton/>
+      <SignInButton/>
     </div>
   )
 }
